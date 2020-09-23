12014e2d381d054675c94b8e88236634e06da4e0
Germain-L <germain.leignel@gmail.com>
2020-09-23 18:55:16 +0200

    fixes

--- fingSwipeV2/lib/pages/end/normal.dart
+++ fingSwipeV2/lib/pages/end/normal.dart
@@ -35,75 +35,75 @@ class _NormalEndState extends State<NormalEnd> {
     return Scaffold(
       appBar: CustomAppBar(),
       body: SizedBox.expand(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(25.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border(
-                  top: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  left: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  right: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "BEST SCORE",
-                    style: TextStyle(fontSize: 33),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border(
+                    top: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    left: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    right: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
                   ),
-                  FutureBuilder<int>(
-                    future: localScore.getHighScore(),
-                    builder:
-                        (BuildContext context, AsyncSnapshot<int> snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(
-                          snapshot.data.toString(),
-                          style: TextStyle(fontSize: 50),
-                        );
-                      }
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "BEST SCORE",
+                      style: TextStyle(fontSize: 33),
+                    ),
+                    FutureBuilder<int>(
+                      future: localScore.getHighScore(),
+                      builder:
+                          (BuildContext context, AsyncSnapshot<int> snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data.toString(),
+                            style: TextStyle(fontSize: 50),
+                          );
+                        }
 
-                      return SizedBox();
-                    },
-                  ),
-                ],
-              ),
-            ), 
-            Container (
-              padding: const EdgeInsets.all(25.0),
-              margin: const EdgeInsets.only(top: 35.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border(
-                  top: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  left: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  right: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                        return SizedBox();
+                      },
+                    ),
+                  ],
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "LAST SCORE",
-                    style: TextStyle(fontSize: 33),
+              Container(
+                padding: const EdgeInsets.all(25.0),
+                margin: const EdgeInsets.only(top: 35.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border(
+                    top: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    left: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    right: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
                   ),
-                  Text(
-                    game.score.toString(),
-                    style: TextStyle(fontSize: 50),
-                  ),
-                ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "LAST SCORE",
+                      style: TextStyle(fontSize: 33),
+                    ),
+                    Text(
+                      game.score.toString(),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
+              Container(
                 margin: EdgeInsets.only(top: 50.0),
                 child: FlatButton(
                   shape: RoundedRectangleBorder(
@@ -132,33 +132,35 @@ class _NormalEndState extends State<NormalEnd> {
                     "Add on leaderboard",
                     style: TextStyle(fontSize: 20.0),
                   ),
-                )),
-            Container(
-              margin: EdgeInsets.only(top: 25.0),
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-                color: Color(0xFF000000),
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.only(
-                    bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
-                splashColor: Color(0xFF1F1F1F),
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, 'menu', (route) => false);
-                },
-                child: Text(
-                  "Play Again",
-                  style: TextStyle(fontSize: 20.0),
                 ),
               ),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  color: Color(0xFF000000),
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.only(
+                      bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
+                  splashColor: Color(0xFF1F1F1F),
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, 'menu', (route) => false);
+                  },
+                  child: Text(
+                    "Play Again",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
