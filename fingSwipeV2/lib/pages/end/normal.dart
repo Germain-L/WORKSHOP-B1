927fcd4bc7c0c270f89bfdc0ed00a9c0edb0d314
Quentin Sar <sarquentin@gmail.com>
2020-09-23 11:08:33 +0100

    Update

--- fingSwipeV2/lib/pages/end/normal.dart
+++ fingSwipeV2/lib/pages/end/normal.dart
@@ -1,4 +1,5 @@
 import 'package:fingSwipeV2/providers/game_provider.dart';
+import 'package:fingSwipeV2/providers/navigation_provider.dart';
 import 'package:fingSwipeV2/widgets/core_widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,113 +10,117 @@ class NormalEnd extends StatelessWidget {
   Widget build(BuildContext context) {
 
     var game = Provider.of<Game>(context);
+    final nav = Provider.of<Nav>(context);
     
-    return SizedBox.expand(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(25.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border(
-                  top: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  left: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  right: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: SizedBox.expand(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border(
+                    top: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    left: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    right: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "BEST SCORE",
+                      style: TextStyle(fontSize: 33),
+                    ),
+                    Text(
+                      "90",
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ]
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "BEST SCORE",
-                    style: TextStyle(fontSize: 33),
+              Container(
+                padding: const EdgeInsets.all(25.0),
+                margin: const EdgeInsets.only(top: 35.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border(
+                    top: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    left: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    right: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                    bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
                   ),
-                  Text(
-                    "90",
-                    style: TextStyle(fontSize: 50),
-                  ),
-                ]
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(25.0),
-              margin: const EdgeInsets.only(top: 35.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border(
-                  top: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  left: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  right: BorderSide(width: 2.0, color: Color(0xFF000000)),
-                  bottom: BorderSide(width: 2.0, color: Color(0xFF000000)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "LAST SCORE",
+                      style: TextStyle(fontSize: 33),
+                    ),
+                    Text(
+                      game.score.toString(),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ]
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    "LAST SCORE",
-                    style: TextStyle(fontSize: 33),
-                  ),
-                  Text(
-                    game.score.toString(),
-                    style: TextStyle(fontSize: 50),
-                  ),
-                ]
+              Container(
+                margin: EdgeInsets.only(top: 50.0),
+                child:
+                  FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    color: Color(0xFFEEEEEE),
+                    textColor: Color(0xFF0E0E0E),
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.only(bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
+                    splashColor: Color(0xFFEDEDED),
+                    onPressed: () {
+                      /*...*/
+                    },
+                    child: Text(
+                      "Add on leaderboard",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 50.0),
-              child:
-                FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  color: Color(0xFFEEEEEE),
-                  textColor: Color(0xFF0E0E0E),
-                  disabledColor: Colors.grey,
-                  disabledTextColor: Colors.black,
-                  padding: EdgeInsets.only(bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
-                  splashColor: Color(0xFFEDEDED),
-                  onPressed: () {
-                    /*...*/
-                  },
-                  child: Text(
-                    "Add on leaderboard",
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                )
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 25.0),
-              child:
-                FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  color: Color(0xFF000000),
-                  textColor: Colors.white,
-                  disabledColor: Colors.grey,
-                  disabledTextColor: Colors.black,
-                  padding: EdgeInsets.only(bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
-                  splashColor: Color(0xFF1F1F1F),
-                  onPressed: () {
-                    /*...*/
-                  },
-                  child: Text(
-                    "Play Again",
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                )
-            ),
-          ],
-        ),
-      )
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                child:
+                  FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    color: Color(0xFF000000),
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.only(bottom: 15.0, top: 15.0, left: 25.0, right: 25.0),
+                    splashColor: Color(0xFF1F1F1F),
+                    onPressed: () {
+                      nav.changePage(0);
+                    },
+                    child: Text(
+                      "Play Again",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )
+              ),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
