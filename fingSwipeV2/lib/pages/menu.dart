87d034a87cf3daa747fd0dfc0f09e90098b87de1
Quentin Sar <sarquentin@gmail.com>
2020-09-24 02:17:28 +0100

    Update

--- fingSwipeV2/lib/pages/menu.dart
+++ fingSwipeV2/lib/pages/menu.dart
@@ -1,31 +1,57 @@
+import 'package:fingSwipeV2/providers/game_provider.dart';
+import 'package:fingSwipeV2/widgets/core_widgets/appbar.dart';
+import 'package:fingSwipeV2/widgets/menu_widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/navigation_provider.dart';
 
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final nav = Provider.of<Nav>(context);
-    return InkWell(
-      onTap: () {
-        nav.changePage(1);
-      },
-      child: Center(
-        child: Transform.scale(
-          scale: 5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+    return Stack(children: <Widget>[
+      Image.asset(
+        "assets/background1.png",
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: CustomAppBar(),
+        body: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, 'normalGame');
+          },
+          child: Stack(
             children: [
-              Icon(LineIcons.play),
-              SizedBox(height: 5),
-              Text("PLAY", style: TextStyle(fontWeight: FontWeight.w700),),
+            Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/play.svg',
+                  height: 75,
+                ),
+                SizedBox(height: 5),
+                Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: Text(
+                    "PLAY",
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 30, color: Color(0xff6ec2bb)),
+                  ),
+                ),
+              ],
+              ),
+            ),
+            Align (
+              alignment: Alignment.bottomCenter,
+              child: MenuButtons()
+            ),
             ],
           ),
         ),
-      ),
-    );
+      )
+    ]);
   }
 }
