fb3f858349d5b5286e7298b1de569c9533907818
Germain-L <germain.leignel@gmail.com>
2020-09-24 10:13:09 +0200

    fixed end menu overflow

--- fingSwipeV2/lib/main.dart
+++ fingSwipeV2/lib/main.dart
@@ -16,9 +16,6 @@ var ap = new AudioCache(fixedPlayer: audioPlayer);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // ap.clearCache();
-  // await ap.loop('music/background.mp3', isNotification: false);
-  // await ap.play('music/background.mp3', isNotification: false);
   runApp(
     MultiProvider(
       providers: [
