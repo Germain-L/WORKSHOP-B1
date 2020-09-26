32f4e8712d4270e40967fafe719bab53f333c122
Germain-L <germain.leignel@gmail.com>
2020-09-26 11:39:24 +0200

    added splashcreen locked fllscreen / orientation

--- fingSwipeV2/lib/main.dart
+++ fingSwipeV2/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:fingSwipeV2/providers/language_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+
 
 import 'pages/end_game_page.dart';
 import 'pages/game_page.dart';
@@ -10,6 +12,8 @@ import 'providers/game_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIOverlays ([]);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
     MultiProvider(
       providers: [
